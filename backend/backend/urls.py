86cbac5c2eff14dbdd10696592f1a7b588f6eb8d
Nirina Arivelo <ckinohrnjr@gmail.com>
2026-09-08 11:01:15 +0300

    Mettre à jour les URLs pour les fichiers statiques

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,4 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Permet de s'assurer que les fichiers statiques et media sont bien pris en compte
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
