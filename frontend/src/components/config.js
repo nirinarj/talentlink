6bd5b69c1fceb20e441539e98b8b619d9db444ca
Nirina Arivelo <ckinohrnjr@gmail.com>
2026-09-11 23:07:01 +0300

    Correction definitive de l'emplacement de config.js

--- frontend/src/components/config.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = window.location.hostname === 'localhost' 
-  ? 'http://127.0.0.1:8000' 
-  : 'https://plateforme-backend-lm08.onrender.com';
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = "https://plateforme-backend-lm08.onrender.com";
+export const API_URL = window.location.hostname === 'localhost' 
+  ? 'http://127.0.0.1:8000' 
+  : 'https://plateforme-backend-lm08.onrender.com';
